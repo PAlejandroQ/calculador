4361ccc2e39a7696dad2eb9577ded48ad8df7b7b
P. Alejandro Quispe <ppaabblloo4283@gmail.com>
2022-12-25 18:03:16 -0500

    Change Gradle

--- calculador/src/main/java/com/desarrollo/calculador/CalculadorApplication.java
+++ calculador/src/main/java/com/desarrollo/calculador/CalculadorApplication.java
@@ -1,4 +1,4 @@
-package com.checha.calculador;
+package com.desarrollo.calculador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- calculador/src/test/java/com/desarrollo/calculador/CalculadorApplicationTests.java
+++ calculador/src/test/java/com/desarrollo/calculador/CalculadorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.checha.calculador;
+package com.desarrollo.calculador;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
