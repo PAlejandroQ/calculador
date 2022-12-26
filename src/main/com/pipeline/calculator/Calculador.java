17e8ac9a19c123033f3d457a65e4eb7d2986f1b5
P. Alejandro Quispe <ppaabblloo4283@gmail.com>
2022-12-26 09:14:53 -0500

    TestImplementation

--- src/main/com/pipeline/calculator/Calculador.java
+++ src/main/com/pipeline/calculator/Calculador.java
@@ -1,5 +1,6 @@
 package com.pipeline.calculator;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Service;
 
 //@SpringBootApplication
@@ -8,9 +9,9 @@ public class Calculador {
 	public int sum(int a, int b) {
 		return a + b;
 	}
-//	public static void main(String[] args) {
-//		SpringApplication.run(CalculatorApplication.class, args);
-//	}
-}
 
+}
+	public static void main(String[] args) {
+		SpringApplication.run(Calculador.class, args);
+	}
 
