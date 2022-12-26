fea1d1ed1ebbac21300a2d8dae098e4084dc25b6
P. Alejandro Quispe <ppaabblloo4283@gmail.com>
2022-12-26 12:25:14 -0500

    Agrega suma logica, controlador y prueba unitaria

---
+++ src/test/java/com/palejandroq/calculador/CalculadorTest.java
@@ -0,0 +1,11 @@
+package com.palejandroq.calculador;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculadorTest {
+    private Calculador calculador = new Calculador();
+    @Test
+    public void testSum() {
+        assertEquals(5, calculador.sum(2, 3));
+    }
+}
